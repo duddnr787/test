0f91c164708cbb464a8699dbce46cbc8d7adaebd
김영욱 <duddnr787@naver.com>
2022-08-05 17:03:08 +0900

    test수정

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     "강남 우동 맛집",
     "파이썬 독학",
   ]);
-  let [좋아요, 좋아요변경] = useState([0,0,0]);
+  let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-  let [title, setTitle] = useState(0);
-  let [입력값, 입력값변경] = useState('');
+  let [title, setTitle] = useState(0); //
+  let [입력값, 입력값변경] = useState(''); // 수정했gi
 
 
 
@@ -23,52 +23,53 @@ function App() {
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
+        
       </div>
-   
+
       {
         글제목.map(function (a, i) {
           return (
-          <div className="list">
-            <h4 onClick={()=>{setModal(!modal), setTitle(i) }}>{글제목[i]} 
-            <span onClick={(e)=>{
-              e.stopPropagation(); 
-              let copy = [...좋아요];
-              copy[i] = copy[i] + 1;
-              좋아요변경(copy);
-            }}> 😞
-            </span> { 좋아요[i] }
-            </h4>
-   
-            
-            <p>2월 17일 발행</p>
-            <button onClick={(e)=>{
+            <div className="list">
+              <h4 onClick={() => { setModal(!modal), setTitle(i) }}>{글제목[i]}
+                <span onClick={(e) => {
+                  e.stopPropagation();
+                  let copy = [...좋아요];
+                  copy[i] = copy[i] + 1;
+                  좋아요변경(copy);
+                }}> 😞
+                </span> {좋아요[i]}
+              </h4>
+
+
+              <p>2월 17일 발행</p>
+              <button onClick={(e) => {
                 let copy = [...글제목];
                 copy.splice(i, 1);
                 글제목변경(copy);
-            }}>삭제</button>
-          </div>
+              }}>삭제</button>
+            </div>
           )
         })
       }
 
-          <input onChange={(e)=>{입력값변경(e.target.value)}} value={입력값} />
-          <button onClick={(e)=>{
-                  let copy = [...글제목]
-                  copy.unshift(입력값)
-                  글제목변경(copy)
-                  입력값변경('')
-          }} >글발행</button>
+      <input onChange={(e) => { 입력값변경(e.target.value) }} value={입력값} />
+      <button onClick={(e) => {
+        let copy = [...글제목]
+        copy.unshift(입력값)
+        글제목변경(copy)
+        입력값변경('')
+      }} >글발행</button>
 
       {
-        modal == true ? <Modal 글제목={글제목} 바꾸기={바꾸기} title={title}/> : null
+        modal == true ? <Modal 글제목={글제목} 바꾸기={바꾸기} title={title} /> : null
       }
     </div>
   );
-  function 바꾸기(){
+  function 바꾸기() {
     let copy = [...글제목];
     copy[0] = "여자 코트 추천";
     글제목변경(copy);
-    }
+  }
 }
 
 
@@ -79,7 +80,7 @@ function Modal(props) {
       <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button onClick={()=>{props.바꾸기()}}>글수정</button>
+      <button onClick={() => { props.바꾸기() }}>글수정</button>
     </div>
   );
 }
